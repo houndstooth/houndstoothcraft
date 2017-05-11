a0ae2a220e7ba04c419126b79fbbe3b1587b4769
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-11 10:51:54 +0900

    share solid square code
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

---
+++ shared/drawSolidSquare.js
@@ -0,0 +1,25 @@
+import { UNIT } from '../shared/customize'
+
+export default ({ctx, origin, size}) => {
+	ctx.beginPath()
+
+	ctx.moveTo(
+		origin[ 0 ],
+		origin[ 1 ]
+	)
+	ctx.lineTo(
+		origin[ 0 ] + (UNIT * size),
+		origin[ 1 ]
+	)
+	ctx.lineTo(
+		origin[ 0 ] + (UNIT * size),
+		origin[ 1 ] + (UNIT * size)
+	)
+	ctx.lineTo(
+		origin[ 0 ],
+		origin[ 1 ] + (UNIT * size)
+	)
+
+	ctx.closePath()
+	ctx.fill()
+}

849ca862b59418eaf622ec201b9e0801a999e91a
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-12 16:49:55 +0900

    iterator one indexed actually works
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- shared/utilities/iterator.js
+++ shared/utilities/iterator.js
@@ -1,6 +1,6 @@
 export default (iterations, options = {oneIndexed: false, countDown: false}) => {
 	let iterator = [ ...Array(iterations).keys() ]
-	if (options.oneIndexed) iterator.map(k => k + 1)
+	if (options.oneIndexed) iterator = iterator.map(k => k + 1)
 	if (options.countDown) iterator.reverse()
 	return iterator
 }

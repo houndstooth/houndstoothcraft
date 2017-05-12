0d9fdfbcb6421208a3486aab1f51f79c0e267540
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-12 14:22:42 +0900

    share square size
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- shared/utilities/iterator.js
+++ shared/utilities/iterator.js
@@ -1,5 +1,6 @@
-export default (iterations, options = {oneIndexed: false}) => {
-	const iterator = [ ...Array(iterations).keys() ]
-	if (options.oneIndexed) return iterator.map(k => k + 1)
+export default (iterations, options = {oneIndexed: false, countDown: false}) => {
+	let iterator = [ ...Array(iterations).keys() ]
+	if (options.oneIndexed) iterator.map(k => k + 1)
+	if (options.countDown) iterator.reverse()
 	return iterator
 }

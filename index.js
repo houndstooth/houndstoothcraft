9c273d52a766d6676835f516ed9ee3d4aeac8e6c
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-12 15:58:56 +0900

    Add gongram submodule
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import { cmyktooth } from './cmyktooth/index'
 import { houndsmorphosis } from './houndsmorphosis/index'
 import { standard } from './standard/index'
 import { ginghamChevronContinuum } from './gingham-chevron-continuum/index'
+import { gongram } from './gongram/index'
 
 // cmyktooth()
 // houndsmorphosis()
 // standard()
-ginghamChevronContinuum()
+// ginghamChevronContinuum()
+gongram()

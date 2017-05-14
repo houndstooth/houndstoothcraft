7dd2bdb60a1673958e17c24e15d1e5c095e39b05
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-14 15:10:32 +0900

    add houndazzle repo

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import { houndsmorphosis } from './houndsmorphosis/index'
 import { standard } from './standard/index'
 import { ginghamChevronContinuum, ginghamChevronContinuumAnimated } from './gingham-chevron-continuum/index'
 import { gongram } from './gongram/index'
+import { houndazzle } from './houndazzle/index'
+
 
 // cmyktooth()
 // houndsmorphosis()
 // standard()
 // ginghamChevronContinuum()
-ginghamChevronContinuumAnimated()
+// ginghamChevronContinuumAnimated()
 // gongram()
+houndazzle()

532b43a0e889847d0c404c8e791fcd3f2b200381
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-14 17:10:00 +0900

    support an extra canvas for pattern creation

--- shared/render/render.js
+++ shared/render/render.js
@@ -1,12 +1,14 @@
 import ctx from './ctx'
 
-export default ({ color, coordinates }) => {
-	ctx.fillStyle = color
-	ctx.beginPath()
+export default ({ color, coordinates, customContext }) => {
+    const context = customContext ? customContext : ctx
 
-	ctx.moveTo(coordinates[ 0 ][ 0 ], coordinates[ 0 ][ 1 ])
-	coordinates.forEach(coordinate => ctx.lineTo(coordinate[ 0 ], coordinate[ 1 ]))
+    context.fillStyle = color
+    context.beginPath()
 
-	ctx.closePath()
-	ctx.fill()
+    context.moveTo(coordinates[ 0 ][ 0 ], coordinates[ 0 ][ 1 ])
+    coordinates.forEach(coordinate => context.lineTo(coordinate[ 0 ], coordinate[ 1 ]))
+
+    context.closePath()
+    context.fill()
 }

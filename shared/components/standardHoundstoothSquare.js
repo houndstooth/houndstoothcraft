1c7c85af11984f8bd8725907a2dc35c06385d9e9
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-17 14:43:34 +0900

    remove principal and minor diagonal; assume minor and rotate arbitrarily
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- shared/components/standardHoundstoothSquare.js
+++ shared/components/standardHoundstoothSquare.js
@@ -1,14 +1,22 @@
-import drawStripedSquare from '../render/drawMinorDiagonalStripedSquare'
+import drawStripedSquare from '../render/drawStripedSquare'
 import drawSolidSquare from '../render/drawSolidSquare'
 import { COLOR_A, COLOR_B } from '../common/customize'
 
-export default ({ origin, size, squareType, scaleFromGridCenter }) => {
+export default ({ origin, size, squareType, scaleFromGridCenter, rotationAboutCenter, rotationAboutOrigin }) => {
 	size = size || 1
-	if (squareType == "STRIPED_A") {
-		drawStripedSquare({ origin, size, originColor: COLOR_A, otherColor: COLOR_B, scaleFromGridCenter })
-	} else if (squareType == "STRIPED_B") {
-		drawStripedSquare({ origin, size, originColor: COLOR_B, otherColor: COLOR_A, scaleFromGridCenter })
-	} else {
-		drawSolidSquare({ origin, size, color: squareType, scaleFromGridCenter })
-	}
+	const squareFunction = squareType == 'STRIPED_A' || squareType == 'STRIPED_B' ? drawStripedSquare : drawSolidSquare
+	const color = squareType
+	const originColor = squareType == 'STRIPED_A' ? COLOR_A : COLOR_B
+	const otherColor = originColor == COLOR_A ? COLOR_B : COLOR_A
+
+	squareFunction({
+		origin,
+		size,
+		color,
+		originColor,
+		otherColor,
+		scaleFromGridCenter,
+		rotationAboutCenter,
+		rotationAboutOrigin
+	})
 }

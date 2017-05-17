af0ae8316419b5bdb300de71e783b0c05722889c
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-17 10:57:38 +0900

    pws config
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+	devtool: 'source-map',
+	entry: './index.js',
+	output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js' },
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: { presets: ['es2015'] }
+			}
+		]
+	},
+}

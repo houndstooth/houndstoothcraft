3290d5a839ac047cdb35524d69d45df4a4ce067f
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-17 16:43:25 +0900

    remove solid square in favor of striped square where colors are the same
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- shared/common/constants.js
+++ shared/common/constants.js
@@ -1,16 +1,49 @@
 import { CANVAS_SIZE, COLOR_A, COLOR_B } from './customize'
 
 const CENTER = [ CANVAS_SIZE / 2, CANVAS_SIZE / 2 ]
+
 const STANDARD_SUPERTILE = [
 	[
-		"STRIPED_A",
-		COLOR_B
+		{
+			originColor: COLOR_A,
+			otherColor: COLOR_A
+		},
+		{
+			originColor: COLOR_B,
+			otherColor: COLOR_A
+		}
 	],
 	[
-		COLOR_A,
-		"STRIPED_B"
+		{
+			originColor: COLOR_A,
+			otherColor: COLOR_B
+		},
+		{
+			originColor: COLOR_B,
+			otherColor: COLOR_B
+		}
 	]
 ]
+
+const SQUARE_TYPE_TO_COLORS_MAPPING = {
+	"SOLID_A": {
+		originColor: COLOR_A,
+		otherColor: COLOR_A
+	},
+	"SOLID_B": {
+		originColor: COLOR_B,
+		otherColor: COLOR_B
+	},
+	"STRIPED_A": {
+		originColor: COLOR_A,
+		otherColor: COLOR_B
+	},
+	"STRIPED_B": {
+		originColor: COLOR_B,
+		otherColor: COLOR_A
+	}
+}
+
 const SWITCHEROO_SUPERTILE = [
 	[
 		"STRIPED_A",
@@ -37,6 +70,7 @@ const SWITCHEROO_SUPERTILE = [
 		"STRIPED_A"
 	]
 ]
+
 const MINOR_DIAGONAL_OFFSET = 0
 const PRINCIPAL_DIAGONAL_OFFSET = Math.PI / 2
 
@@ -45,5 +79,6 @@ export {
 	STANDARD_SUPERTILE,
 	SWITCHEROO_SUPERTILE,
 	MINOR_DIAGONAL_OFFSET,
-	PRINCIPAL_DIAGONAL_OFFSET
+	PRINCIPAL_DIAGONAL_OFFSET,
+	SQUARE_TYPE_TO_COLORS_MAPPING
 }
--- shared/components/standardHoundstoothSquare.js
+++ shared/components/standardHoundstoothSquare.js
@@ -1,22 +1,18 @@
 import drawStripedSquare from '../render/drawStripedSquare'
-import drawSolidSquare from '../render/drawSolidSquare'
-import { COLOR_A, COLOR_B } from '../common/customize'
 import maybeFlipGrain from '../utilities/maybeFlipGrain'
 
-export default ({ origin, center, size, squareType, scaleFromGridCenter, rotationAboutCenter, rotationAboutOrigin }) => {
+export default ({ origin, center, size, originColor: initialOriginColor, otherColor: initialOtherColor, scaleFromGridCenter, rotationAboutCenter, rotationAboutOrigin }) => {
 	size = size || 1
-	const squareFunction = squareType === 'STRIPED_A' || squareType === 'STRIPED_B' ? drawStripedSquare : drawSolidSquare
-	const color = squareType
-	const initialOriginColor = squareType === 'STRIPED_A' ? COLOR_A : COLOR_B
-	const initialOtherColor = initialOriginColor === COLOR_A ? COLOR_B : COLOR_A
 
-	const { originColor, otherColor } = maybeFlipGrain({originColor: initialOriginColor, otherColor: initialOtherColor})
+	const { originColor, otherColor } = maybeFlipGrain({
+		originColor: initialOriginColor,
+		otherColor: initialOtherColor
+	})
 
-	squareFunction({
+	drawStripedSquare({
 		origin,
 		center,
 		size,
-		color,
 		originColor,
 		otherColor,
 		scaleFromGridCenter,

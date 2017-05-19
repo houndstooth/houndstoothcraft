2db8d9375f1a4517e882207c2e27d2aa4110b030
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-19 14:33:07 +0900

    restore some harmonitooth / gcc stuff we lost in that last beig refactor
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import { houndsmorphosis } from './houndsmorphosis/index'
 import { standard } from './standard/index'
 import { ginghamChevronContinuum } from './gingham-chevron-continuum/index'
 import { ginghamChevronContinuumAnimated } from './gingham-chevron-continuum-animated/index'
+import { harmonitooth } from './harmonitooth/index'
 import { houndazzle } from './houndazzle/index'
 
 
 // cmyktooth()
 // houndsmorphosis()
-standard()
+// standard()
 // ginghamChevronContinuum()
 // ginghamChevronContinuumAnimated()
+harmonitooth()
 // houndazzle()

90552f408a51d5b954756fa314b2f32c5bdae022
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-19 14:02:55 +0900

    static gcc improvements
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- shared/utilities/calculateStripes.js
+++ shared/utilities/calculateStripes.js
@@ -1,18 +1,23 @@
-import { DERASTERIZED_BY_AREA, HARMONIZE_STRIPE_WIDTH } from '../common/customize'
+import { STRIPE_STYLE } from '../common/customize'
 import iterator from './iterator'
-import calculateHarmonicStripe from '../../harmonitooth/utilities/calculateHarmonicStripe'
+import calculateHarmonicContinuumSegmentStripe from '../../harmonitooth/utilities/calculateHarmonicContinuumSegmentStripe'
+import calculateHarmonicContinuumStripe from '../../harmonitooth/utilities/calculateHarmonicContinuumStripe'
 import calculateDerasterizedByAreaStripe from '../../derasterized/utilities/calculateDerasterizedByAreaStripe'
 
 const PERIMETER_SCALAR = 2
 
 export default ({ stripeCount }) => iterator(stripeCount).map(stripeIndex => {
 	let stripe
-	if (DERASTERIZED_BY_AREA) {
+	if (STRIPE_STYLE === 'DERASTERIZED_BY_AREA') {
 		stripe = calculateDerasterizedByAreaStripe({ stripeCount, stripeIndex })
-	} else if (HARMONIZE_STRIPE_WIDTH) {
-		stripe = calculateHarmonicStripe({ stripeCount, stripeIndex })
-	} else {
+	} else if (STRIPE_STYLE === 'SEGMENT_OF_HARMONIC_CONTINUUM_ACROSS_GRID') {
+		stripe = calculateHarmonicContinuumSegmentStripe({ stripeCount, stripeIndex })
+	} else if (STRIPE_STYLE === 'FULL_HARMONIC_CONTINUUM_COMPRESSED_INTO_SINGLE_TILE') {
+		stripe = calculateHarmonicContinuumStripe({ stripeCount, stripeIndex })
+	} else if (STRIPE_STYLE === 'STANDARD') {
 		stripe = stripeIndex / stripeCount
+	} else {
+		console.log('stripe style not set!')
 	}
 
 	return stripe * PERIMETER_SCALAR

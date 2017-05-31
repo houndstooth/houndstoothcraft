3cc87247259d844b1f1123f246e8b2fef1e3ad29
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-31 11:37:24 +0900

    de-deify tile: extract off screen check
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- shared/components/tile.js
+++ shared/components/tile.js
@@ -14,6 +14,7 @@ import rotateCoordinateAboutPoint from '../utilities/rotateCoordinateAboutPoint'
 import calculateOriginAndCenter from '../utilities/calculateOriginAndCenter'
 import calculateHoundazzleSolidTileSubstripeCoordinates from '../../houndazzle/calculateHoundazzleSolidTileSubstripeCoordinates'
 import calculateSubstripeStripeUnionCoordinates from '../../houndazzle/calculateSubstripeStripeUnionCoordinates'
+// import isOnCanvas from '../utilities/isOnCanvas'
 
 const PERIMETER_SCALAR = 2
 
@@ -308,33 +309,6 @@ const maybeSwitcherooColors = ({ colors, origin }) => {
 
 	return colors
 }
-
-// const isOnCanvas = ({ center, sizedUnit }) => {
-// 	let vertices = calculateSquareCoordinates({ center, sizedUnit })
-//
-// 	const { canvasSize, gridRotationAboutCenter } = state.shared
-// 	const canvasCenter = [ canvasSize / 2, canvasSize / 2]
-//
-// 	if (state.shared.gridRotationAboutCenter) {
-// 		vertices = rotateCoordinatesAboutPoint({
-// 			point: canvasCenter,
-// 			coordinates: vertices,
-// 			rotation: gridRotationAboutCenter
-// 		})
-// 	}
-//
-// 	return vertices.some(pointIsOnCanvas)
-//
-// 	// it's way more complex than this, though...
-// 	// need to handle the cases when:
-// 	// 1. all vertices are off, but an edge is still on
-// 	// 2. this one tile takes up the entire canvas (but all its edges and vertices are off)
-// }
-
-// const pointIsOnCanvas = point => {
-// 	const canvasSize = state.shared.canvasSize
-// 	return point[0] >= 0 && point[0] < canvasSize && point[1] >= 0 && point[1] < canvasSize
-// }
 
 const colorsAreTheSame = ({ colors }) => {
 	const colorOne = colors[ 0 ]
---
+++ shared/utilities/isOnCanvas.js
@@ -0,0 +1,26 @@
+// const pointIsOnCanvas = point => {
+// 	const canvasSize = state.shared.canvasSize
+// 	return point[0] >= 0 && point[0] < canvasSize && point[1] >= 0 && point[1] < canvasSize
+// }
+
+// export default ({ center, sizedUnit }) => {
+// 	let vertices = calculateSquareCoordinates({ center, sizedUnit })
+//
+// 	const { canvasSize, gridRotationAboutCenter } = state.shared
+// 	const canvasCenter = [ canvasSize / 2, canvasSize / 2]
+//
+// 	if (state.shared.gridRotationAboutCenter) {
+// 		vertices = rotateCoordinatesAboutPoint({
+// 			point: canvasCenter,
+// 			coordinates: vertices,
+// 			rotation: gridRotationAboutCenter
+// 		})
+// 	}
+//
+// 	return vertices.some(pointIsOnCanvas)
+//
+// 	// it's way more complex than this, though...
+// 	// need to handle the cases when:
+// 	// 1. all vertices are off, but an edge is still on
+// 	// 2. this one tile takes up the entire canvas (but all its edges and vertices are off)
+// }

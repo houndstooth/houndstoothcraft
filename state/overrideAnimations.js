25aecb0338f9ca7453e2efae61cff5914c206121
Person McPivot <person.mcpivot@pivotal.io>
2017-06-07 10:42:11 +0900

    Revert "well on the right track but major yikes"
    This reverts commit 6eb024fcfeae4e12373d04605ce3bde7f6232ded.

--- state/overrideAnimations.js
+++ state/overrideAnimations.js
@@ -1,14 +1,13 @@
 // import { ANIMATION_RATE } from '../application/constants'
-// import currentAnimation from '../../state/currentAnimation'
 
 export default {
-	// viewConfig: {
-	// 	zoom: () => () => state.viewConfig.zoom * Math.pow(ANIMATION_RATE, currentAnimation.i)
+	// view: {
+	// 	zoom: p => p * ANIMATION_RATE
 	// },
 	// stripeCountConfig: {
-	// 	stripeCount: () => state.stripeCountConfig.stripeCount * Math.pow(ANIMATION_RATE / 10, currentAnimation.i)
+	// 	stripeCount: p => p * ANIMATION_RATE / 10
 	// },
 	// grid: {
-	// 	gridRotationAboutGridCenter: () => currentAnimation.i * Math.PI / 360
+	// 	gridRotationAboutGridCenter: p => p + Math.PI / 360
 	// }
 }
--- state/overrideIterations.js
+++ state/overrideIterations.js
@@ -1,11 +1,8 @@
-// import currentIteration from './currentIteration'
-// import state from './state'
-
 export default {
-	// tileConfig: {
-	// 	tileSize: () => state.tileConfig.tileSize * Math.pow(0.5, currentIteration.i)
+	// tile: {
+	// 	tileSize: p => p / 2,
 	// },
 	// colorConfig: {
-	// 	opacity: () => state.colorConfig.opacity * Math.pow(0.5, currentIteration.i)
+	// 	opacity: p => p / 2
 	// }
 }

7f22242d882bc67ffc0dc777106347f42f5ebece
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-08 17:50:36 +0900

    back to the past, before elm
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- index.js
+++ index.js
@@ -9,17 +9,6 @@ import houndazzleContinuumEffect from './effects/houndazzle/houndazzleContinuumE
 import gongramEffect from './effects/gongram/gongramEffect'
 import houndsmorphosisEffect from './effects/houndsmorphosis/houndsmorphosisEffect'
 
-const callMethodsToPreventReformattingToolsFromCleaningUpUnusedImportStatements = () => {
-	cmyktoothEffect()
-	ginghamChevronContinuumEffect()
-	harmonitoothEffect()
-	houndazzleEffect()
-	houndazzleContinuumEffect()
-	gongramEffect()
-	houndsmorphosisEffect()
-}
-callMethodsToPreventReformattingToolsFromCleaningUpUnusedImportStatements()
-
 setup({
 	effects: [],
 	debugging: false

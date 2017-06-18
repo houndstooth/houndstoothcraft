0f0a5b1b6280568c761027b1e0b5224c1109a5ae
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-19 00:27:29 +0900

    WIP stripes

--- test/integration/standardTest.js
+++ test/integration/standardTest.js
@@ -23,23 +23,59 @@ describe("Standard Houndstooth", () => {
         })
     })
 
-    it("proves the concept of testing with pixel data", () => {
-        const something = ctx.getImageData(50,0,1,1)
-        console.log(
-            'pixel at [50, 0] - ',
-            'r:', something.data[0], 
-            'g:', something.data[1], 
-            'b:', something.data[2], 
-            'a:', something.data[3]
-        )
-
-        expect(true).toBe(true)
-    })
-
     it("proves the concept of testing with call stack to canvas", () => {
         const stack = ctx.stack()
         console.log('canvas calls:', stack)
 
         expect(true).toBe(true)
     })
+
+    it("has four stripes in a striped square", () => {
+        //should this be more like just draw a line?
+        //or should i rely more on stripe coordinates unit test?
+        //maybe you should just go diagonally across perp to the stripes, just check 4 spots
+        //or maybe check some set using like the same subdivision as you do for othertooth
+        expect(pixel(sectorCenter({x: 0, y: 0, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 1, y: 0, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 0, y: 1, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+
+        expect(pixel(sectorCenter({x: 3, y: 0, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 2, y: 0, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 2, y: 1, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 1, y: 1, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 1, y: 2, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 0, y: 2, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 0, y: 3, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+
+        expect(pixel(sectorCenter({x: 1, y: 4, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 2, y: 4, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 2, y: 3, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 3, y: 3, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 3, y: 2, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 4, y: 2, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+        expect(pixel(sectorCenter({x: 4, y: 1, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 0})
+
+        expect(pixel(sectorCenter({x: 3, y: 4, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 4, y: 3, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+        expect(pixel(sectorCenter({x: 4, y: 4, n: 5}))).toEqual({r: 0, g: 0, b: 0, a: 1})
+    })
 })
+
+const tileOrigin = (x, y) => {
+    return 
+}
+
+const pixel = ([x, y]) => {
+    const pixelData = ctx.getImageData(x, y, 1, 1).data
+    return {
+        r: pixelData[0], 
+        g: pixelData[1], 
+        b: pixelData[2], 
+        a: pixelData[3] / 255
+    }
+}
+
+const sectorCenter = ({x, y, n}) => {
+    const sectorSize = state.tileConfig.tileSize / n
+    return [ (x + 0.5) * sectorSize, (y + 0.5) * sectorSize ]
+}

3daf8c73ccc422ca9fc184f6ab664e142a113993
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-21 00:39:23 +0900

    backfill clear test

---
+++ test/render/clearTest.js
@@ -0,0 +1,16 @@
+import 'jasmine'
+
+import clear from '../../render/clear'
+import ctx from '../../render/ctx'
+import state from '../../state/state'
+
+describe('clear', () => {
+    it('wipes the entire canvas', () => {
+        spyOn(ctx, 'clearRect')
+        state.viewConfig.canvasSize = 500
+
+        clear()
+
+        expect(ctx.clearRect).toHaveBeenCalledWith(0, 0, 500, 500)
+    })
+})

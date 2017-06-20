b6cacfa7f099e7866a564f28ef1153f031859aa6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-21 00:36:11 +0900

    backfill setup canvas test

---
+++ test/render/setupCanvasTest.js
@@ -0,0 +1,16 @@
+import 'jasmine'
+
+import setupCanvas from '../../render/setupCanvas'
+import state from '../../state/state'
+import canvas from '../../render/canvas'
+
+describe('setupCanvas', () => {
+    it('sets the canvas\'s width and height with config', () => {
+        state.viewConfig.canvasSize = 400
+
+        setupCanvas()
+
+        expect(canvas.width).toEqual(400)
+        expect(canvas.height).toEqual(400)
+    })
+})

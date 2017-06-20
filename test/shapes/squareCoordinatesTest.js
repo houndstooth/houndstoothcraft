c3f55e0f2de2d1b762c31c77e2d9ede40101c58f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-20 21:39:26 +0900

    backfill test for square coordinates

---
+++ test/shapes/squareCoordinatesTest.js
@@ -0,0 +1,17 @@
+import 'jasmine'
+
+import squareCoordinates from '../../shapes/squareCoordinates'
+
+const tileOrigin = [ 2, 3 ]
+const sizedUnit = 5
+
+describe("square coordinates", () => {
+    it('makes a square in the correct place of the correct size', () => {
+        expect(squareCoordinates({ tileOrigin, sizedUnit })).toEqual([
+            [ 2, 3 ],
+            [ 7, 3 ],
+            [ 7, 8 ],
+            [ 2, 8 ]
+        ])
+    })
+})

08b4fbc640441194e4cb5443ca4b7363cd09a37e
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-29 09:02:04 +0900

    flatten execute test
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- test/executeTest.js
+++ test/executeTest.js
@@ -116,203 +116,217 @@ describe('execute', () => {
 		})
 	})
 
-	describe('iterating', () => {
-		describe('when iterating', () => {
-			beforeEach(() => {
-				iterating = true
-				settings.initial.iteration = {
-					startIteration: 5,
-					endIteration: 8
-				}
-			})
-
-			describe('animating', () => {
-				describe('when animating', () => {
-					beforeEach(() => {
-						animating = true
-						settings.initial.animation = {
-							startAnimationFrame: 2,
-							endAnimationFrame: 5
-						}
-					})
-
-					it('calls grid once for each iteration within each animation, both inclusively', () => {
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						expect(gridSpy.calls.count()).toBe(16)
-					})
-
-					it('calls animation functions once for each animation, including before rendering starts', () => {
-						const animationFunction = jasmine.createSpy().and.callFake(p => p * 2)
-						settings.initial.exampleConfig = { exampleProperty: 1 }
-						settings.animations.exampleConfig = { exampleProperty: animationFunction }
-
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						const animationFunctionCalls = animationFunction.calls.all()
-						expect(animationFunctionCalls.length).toBe(6)
-						expect(animationFunctionCalls[ 0 ].args[ 0 ]).toBe(1)
-						expect(animationFunctionCalls[ 1 ].args[ 0 ]).toBe(2)
-						expect(animationFunctionCalls[ 2 ].args[ 0 ]).toBe(4)
-						expect(animationFunctionCalls[ 3 ].args[ 0 ]).toBe(8)
-						expect(animationFunctionCalls[ 4 ].args[ 0 ]).toBe(16)
-						expect(animationFunctionCalls[ 5 ].args[ 0 ]).toBe(32)
-					})
-
-					it('handles animation functions of the current animation frame', () => {
-						const animationFunction = jasmine.createSpy().and.callFake(() => 1000 - (current.animation + 1))
-						settings.initial.exampleConfig = { exampleProperty: 1000 }
-						settings.animations.exampleConfig = { exampleProperty: animationFunction }
-
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						const animationFunctionCalls = animationFunction.calls.all()
-						expect(animationFunctionCalls.length).toBe(6)
-						expect(animationFunctionCalls[ 0 ].args[ 0 ]).toBe(1000)
-						expect(animationFunctionCalls[ 1 ].args[ 0 ]).toBe(999)
-						expect(animationFunctionCalls[ 2 ].args[ 0 ]).toBe(998)
-						expect(animationFunctionCalls[ 3 ].args[ 0 ]).toBe(997)
-						expect(animationFunctionCalls[ 4 ].args[ 0 ]).toBe(996)
-						expect(animationFunctionCalls[ 5 ].args[ 0 ]).toBe(995)
-					})
-
-					it('calls iteration functions once for each iteration, each animation frame, starting over each animation frame', () => {
-						settings.initial.exampleConfig = { exampleProperty: 0 }
-
-						const animationFunction = jasmine.createSpy().and.callFake(p => p + 10)
-						settings.animations.exampleConfig = { exampleProperty: animationFunction }
-
-						const iterationFunction = jasmine.createSpy().and.callFake(p => p + 1)
-						settings.iterations.exampleConfig = { exampleProperty: iterationFunction }
-
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						const animationFunctionCalls = animationFunction.calls.all()
-						expect(animationFunctionCalls.length).toBe(6)
-						expect(animationFunctionCalls[ 0 ].args[ 0 ]).toBe(0)
-						expect(animationFunctionCalls[ 1 ].args[ 0 ]).toBe(10)
-						expect(animationFunctionCalls[ 2 ].args[ 0 ]).toBe(20)
-						expect(animationFunctionCalls[ 3 ].args[ 0 ]).toBe(30)
-						expect(animationFunctionCalls[ 4 ].args[ 0 ]).toBe(40)
-						expect(animationFunctionCalls[ 5 ].args[ 0 ]).toBe(50)
-
-						const iterationFunctionCalls = iterationFunction.calls.all()
-						expect(iterationFunctionCalls.length).toBe(36)
-						expect(iterationFunctionCalls[ 0 ].args[ 0 ]).toBe(20)
-						expect(iterationFunctionCalls[ 1 ].args[ 0 ]).toBe(21)
-						expect(iterationFunctionCalls[ 2 ].args[ 0 ]).toBe(22)
-						expect(iterationFunctionCalls[ 3 ].args[ 0 ]).toBe(23)
-						expect(iterationFunctionCalls[ 4 ].args[ 0 ]).toBe(24)
-						expect(iterationFunctionCalls[ 5 ].args[ 0 ]).toBe(25)
-						expect(iterationFunctionCalls[ 6 ].args[ 0 ]).toBe(26)
-						expect(iterationFunctionCalls[ 7 ].args[ 0 ]).toBe(27)
-						expect(iterationFunctionCalls[ 8 ].args[ 0 ]).toBe(28)
-
-						expect(iterationFunctionCalls[ 9 ].args[ 0 ]).toBe(30)
-						expect(iterationFunctionCalls[ 10 ].args[ 0 ]).toBe(31)
-						expect(iterationFunctionCalls[ 11 ].args[ 0 ]).toBe(32)
-						expect(iterationFunctionCalls[ 12 ].args[ 0 ]).toBe(33)
-						expect(iterationFunctionCalls[ 13 ].args[ 0 ]).toBe(34)
-						expect(iterationFunctionCalls[ 14 ].args[ 0 ]).toBe(35)
-						expect(iterationFunctionCalls[ 15 ].args[ 0 ]).toBe(36)
-						expect(iterationFunctionCalls[ 16 ].args[ 0 ]).toBe(37)
-						expect(iterationFunctionCalls[ 17 ].args[ 0 ]).toBe(38)
-
-						expect(iterationFunctionCalls[ 18 ].args[ 0 ]).toBe(40)
-						expect(iterationFunctionCalls[ 19 ].args[ 0 ]).toBe(41)
-						expect(iterationFunctionCalls[ 20 ].args[ 0 ]).toBe(42)
-						expect(iterationFunctionCalls[ 21 ].args[ 0 ]).toBe(43)
-						expect(iterationFunctionCalls[ 22 ].args[ 0 ]).toBe(44)
-						expect(iterationFunctionCalls[ 23 ].args[ 0 ]).toBe(45)
-						expect(iterationFunctionCalls[ 24 ].args[ 0 ]).toBe(46)
-						expect(iterationFunctionCalls[ 25 ].args[ 0 ]).toBe(47)
-						expect(iterationFunctionCalls[ 26 ].args[ 0 ]).toBe(48)
-
-						expect(iterationFunctionCalls[ 27 ].args[ 0 ]).toBe(50)
-						expect(iterationFunctionCalls[ 28 ].args[ 0 ]).toBe(51)
-						expect(iterationFunctionCalls[ 29 ].args[ 0 ]).toBe(52)
-						expect(iterationFunctionCalls[ 30 ].args[ 0 ]).toBe(53)
-						expect(iterationFunctionCalls[ 31 ].args[ 0 ]).toBe(54)
-						expect(iterationFunctionCalls[ 32 ].args[ 0 ]).toBe(55)
-						expect(iterationFunctionCalls[ 33 ].args[ 0 ]).toBe(56)
-						expect(iterationFunctionCalls[ 34 ].args[ 0 ]).toBe(57)
-						expect(iterationFunctionCalls[ 35 ].args[ 0 ]).toBe(58)
-					})
-				})
-
-				describe('when not animating', () => {
-					beforeEach(() => {
-						animating = false
-					})
-
-					it('calls grid once for each iteration between start and end, inclusive', () => {
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						expect(gridSpy.calls.count()).toBe(4)
-					})
-
-					it('calls iteration functions once for each iteration, including before rendering starts', () => {
-						const iterationFunction = jasmine.createSpy().and.callFake(p => p * 2)
-						settings.initial.exampleConfig = { exampleProperty: 1 }
-						settings.iterations.exampleConfig = { exampleProperty: iterationFunction }
-
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						const iterationFunctionCalls = iterationFunction.calls.all()
-						expect(iterationFunctionCalls.length).toBe(9)
-						expect(iterationFunctionCalls[ 0 ].args[ 0 ]).toBe(1)
-						expect(iterationFunctionCalls[ 1 ].args[ 0 ]).toBe(2)
-						expect(iterationFunctionCalls[ 2 ].args[ 0 ]).toBe(4)
-						expect(iterationFunctionCalls[ 3 ].args[ 0 ]).toBe(8)
-						expect(iterationFunctionCalls[ 4 ].args[ 0 ]).toBe(16)
-						expect(iterationFunctionCalls[ 5 ].args[ 0 ]).toBe(32)
-						expect(iterationFunctionCalls[ 6 ].args[ 0 ]).toBe(64)
-						expect(iterationFunctionCalls[ 7 ].args[ 0 ]).toBe(128)
-						expect(iterationFunctionCalls[ 8 ].args[ 0 ]).toBe(256)
-					})
-
-					it('handles iteration functions of the iteration frame (but does not affect the first run for no good reason...)', () => {
-						const iterationFunction = jasmine.createSpy().and.callFake(() => 1000 - (current.iteration + 1))
-						settings.initial.exampleConfig = { exampleProperty: 1000 }
-						settings.iterations.exampleConfig = { exampleProperty: iterationFunction }
-
-						execute({ iterating, animating, exportFrames, performanceLogging })
-
-						const iterationFunctionCalls = iterationFunction.calls.all()
-						expect(iterationFunctionCalls.length).toBe(9)
-						expect(iterationFunctionCalls[ 0 ].args[ 0 ]).toBe(1000)
-						expect(iterationFunctionCalls[ 1 ].args[ 0 ]).toBe(999)
-						expect(iterationFunctionCalls[ 2 ].args[ 0 ]).toBe(998)
-						expect(iterationFunctionCalls[ 3 ].args[ 0 ]).toBe(997)
-						expect(iterationFunctionCalls[ 4 ].args[ 0 ]).toBe(996)
-						expect(iterationFunctionCalls[ 5 ].args[ 0 ]).toBe(995)
-						expect(iterationFunctionCalls[ 6 ].args[ 0 ]).toBe(994)
-						expect(iterationFunctionCalls[ 7 ].args[ 0 ]).toBe(993)
-						expect(iterationFunctionCalls[ 8 ].args[ 0 ]).toBe(992)
-					})
-				})
-			})
+	describe('neither iterating nor animating', () => {
+		beforeEach(() => {
+			iterating = false
+			animating = false
 		})
 
-		describe('when not iterating', () => {
-			beforeEach(() => {
-				iterating = false
-			})
+		it('calls grid only once', () => {
+			execute({ iterating, animating, exportFrames, performanceLogging })
 
-			it('calls grid only once', () => {
-				execute({ iterating, animating, exportFrames, performanceLogging })
+			expect(gridSpy.calls.count()).toBe(1)
+		})
 
-				expect(gridSpy.calls.count()).toBe(1)
-			})
+		it('does not call iteration functions', () => {
+			const iterationFunction = jasmine.createSpy()
+			settings.iterations.exampleProperty = iterationFunction
 
-			it('does not call iteration functions', () => {
-				const iterationFunction = jasmine.createSpy()
-				settings.iterations.exampleProperty = iterationFunction
+			execute({ iterating, animating, exportFrames, performanceLogging })
 
-				execute({ iterating, animating, exportFrames, performanceLogging })
+			expect(iterationFunction).not.toHaveBeenCalled()
+		})
+	})
 
-				expect(iterationFunction).not.toHaveBeenCalled()
-			})
+	describe('iterating (but not animating)', () => {
+		beforeEach(() => {
+			iterating = true
+			animating = false
+			settings.initial.iteration = {
+				startIteration: 5,
+				endIteration: 8
+			}
+		})
+
+		it('calls grid once for each iteration between start and end, inclusive', () => {
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			expect(gridSpy.calls.count()).toBe(4)
+		})
+
+		it('calls iteration functions once for each iteration, including before rendering starts', () => {
+			const iterationFunction = jasmine.createSpy().and.callFake(p => p * 2)
+			settings.initial.exampleConfig = { exampleProperty: 1 }
+			settings.iterations.exampleConfig = { exampleProperty: iterationFunction }
+
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			const iterationFunctionCalls = iterationFunction.calls.all()
+			expect(iterationFunctionCalls.length).toBe(9)
+			expect(iterationFunctionCalls[ 0 ].args[ 0 ]).toBe(1)
+			expect(iterationFunctionCalls[ 1 ].args[ 0 ]).toBe(2)
+			expect(iterationFunctionCalls[ 2 ].args[ 0 ]).toBe(4)
+			expect(iterationFunctionCalls[ 3 ].args[ 0 ]).toBe(8)
+			expect(iterationFunctionCalls[ 4 ].args[ 0 ]).toBe(16)
+			expect(iterationFunctionCalls[ 5 ].args[ 0 ]).toBe(32)
+			expect(iterationFunctionCalls[ 6 ].args[ 0 ]).toBe(64)
+			expect(iterationFunctionCalls[ 7 ].args[ 0 ]).toBe(128)
+			expect(iterationFunctionCalls[ 8 ].args[ 0 ]).toBe(256)
+		})
+
+		it('handles iteration functions of the iteration frame', () => {
+			const iterationFunction = jasmine.createSpy().and.callFake(() => 1000 - (current.iteration + 1))
+			settings.initial.exampleConfig = { exampleProperty: 1000 }
+			settings.iterations.exampleConfig = { exampleProperty: iterationFunction }
+
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			const iterationFunctionCalls = iterationFunction.calls.all()
+			expect(iterationFunctionCalls.length).toBe(9)
+			expect(iterationFunctionCalls[ 0 ].args[ 0 ]).toBe(1000)
+			expect(iterationFunctionCalls[ 1 ].args[ 0 ]).toBe(999)
+			expect(iterationFunctionCalls[ 2 ].args[ 0 ]).toBe(998)
+			expect(iterationFunctionCalls[ 3 ].args[ 0 ]).toBe(997)
+			expect(iterationFunctionCalls[ 4 ].args[ 0 ]).toBe(996)
+			expect(iterationFunctionCalls[ 5 ].args[ 0 ]).toBe(995)
+			expect(iterationFunctionCalls[ 6 ].args[ 0 ]).toBe(994)
+			expect(iterationFunctionCalls[ 7 ].args[ 0 ]).toBe(993)
+			expect(iterationFunctionCalls[ 8 ].args[ 0 ]).toBe(992)
+		})
+	})
+
+	describe('animating (but not iterating)', () => {
+		beforeEach(() => {
+			animating = true
+			iterating = false
+			settings.initial.animation = {
+				startAnimationFrame: 2,
+				endAnimationFrame: 5
+			}
+		})
+
+		it('calls grid once for each animation between start and end, inclusive', () => {
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			expect(gridSpy.calls.count()).toBe(4)
+		})
+
+		it('calls animation functions once for each animation, including before rendering starts', () => {
+			const animationFunction = jasmine.createSpy().and.callFake(p => p * 2)
+			settings.initial.exampleConfig = { exampleProperty: 1 }
+			settings.animations.exampleConfig = { exampleProperty: animationFunction }
+
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			const animationFunctionCalls = animationFunction.calls.all()
+			expect(animationFunctionCalls.length).toBe(6)
+			expect(animationFunctionCalls[ 0 ].args[ 0 ]).toBe(1)
+			expect(animationFunctionCalls[ 1 ].args[ 0 ]).toBe(2)
+			expect(animationFunctionCalls[ 2 ].args[ 0 ]).toBe(4)
+			expect(animationFunctionCalls[ 3 ].args[ 0 ]).toBe(8)
+			expect(animationFunctionCalls[ 4 ].args[ 0 ]).toBe(16)
+			expect(animationFunctionCalls[ 5 ].args[ 0 ]).toBe(32)
+		})
+
+		it('handles animation functions of the current animation frame', () => {
+			const animationFunction = jasmine.createSpy().and.callFake(() => 1000 - (current.animation + 1))
+			settings.initial.exampleConfig = { exampleProperty: 1000 }
+			settings.animations.exampleConfig = { exampleProperty: animationFunction }
+
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			const animationFunctionCalls = animationFunction.calls.all()
+			expect(animationFunctionCalls.length).toBe(6)
+			expect(animationFunctionCalls[ 0 ].args[ 0 ]).toBe(1000)
+			expect(animationFunctionCalls[ 1 ].args[ 0 ]).toBe(999)
+			expect(animationFunctionCalls[ 2 ].args[ 0 ]).toBe(998)
+			expect(animationFunctionCalls[ 3 ].args[ 0 ]).toBe(997)
+			expect(animationFunctionCalls[ 4 ].args[ 0 ]).toBe(996)
+			expect(animationFunctionCalls[ 5 ].args[ 0 ]).toBe(995)
+		})
+	})
+
+	describe('iterating and animating', () => {
+		beforeEach(() => {
+			animating = true
+			iterating = true
+			settings.initial.iteration = {
+				startIteration: 5,
+				endIteration: 8
+			}
+			settings.initial.animation = {
+				startAnimationFrame: 2,
+				endAnimationFrame: 5
+			}
+		})
+
+		it('calls grid once for each iteration within each animation, both inclusively', () => {
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			expect(gridSpy.calls.count()).toBe(16)
+		})
+
+		it('calls iteration functions once for each iteration, each animation frame, starting over each animation frame', () => {
+			settings.initial.exampleConfig = { exampleProperty: 0 }
+
+			const animationFunction = jasmine.createSpy().and.callFake(p => p + 10)
+			settings.animations.exampleConfig = { exampleProperty: animationFunction }
+
+			const iterationFunction = jasmine.createSpy().and.callFake(p => p + 1)
+			settings.iterations.exampleConfig = { exampleProperty: iterationFunction }
+
+			execute({ iterating, animating, exportFrames, performanceLogging })
+
+			const animationFunctionCalls = animationFunction.calls.all()
+			expect(animationFunctionCalls.length).toBe(6)
+			expect(animationFunctionCalls[ 0 ].args[ 0 ]).toBe(0)
+			expect(animationFunctionCalls[ 1 ].args[ 0 ]).toBe(10)
+			expect(animationFunctionCalls[ 2 ].args[ 0 ]).toBe(20)
+			expect(animationFunctionCalls[ 3 ].args[ 0 ]).toBe(30)
+			expect(animationFunctionCalls[ 4 ].args[ 0 ]).toBe(40)
+			expect(animationFunctionCalls[ 5 ].args[ 0 ]).toBe(50)
+
+			const iterationFunctionCalls = iterationFunction.calls.all()
+			expect(iterationFunctionCalls.length).toBe(36)
+			expect(iterationFunctionCalls[ 0 ].args[ 0 ]).toBe(20)
+			expect(iterationFunctionCalls[ 1 ].args[ 0 ]).toBe(21)
+			expect(iterationFunctionCalls[ 2 ].args[ 0 ]).toBe(22)
+			expect(iterationFunctionCalls[ 3 ].args[ 0 ]).toBe(23)
+			expect(iterationFunctionCalls[ 4 ].args[ 0 ]).toBe(24)
+			expect(iterationFunctionCalls[ 5 ].args[ 0 ]).toBe(25)
+			expect(iterationFunctionCalls[ 6 ].args[ 0 ]).toBe(26)
+			expect(iterationFunctionCalls[ 7 ].args[ 0 ]).toBe(27)
+			expect(iterationFunctionCalls[ 8 ].args[ 0 ]).toBe(28)
+
+			expect(iterationFunctionCalls[ 9 ].args[ 0 ]).toBe(30)
+			expect(iterationFunctionCalls[ 10 ].args[ 0 ]).toBe(31)
+			expect(iterationFunctionCalls[ 11 ].args[ 0 ]).toBe(32)
+			expect(iterationFunctionCalls[ 12 ].args[ 0 ]).toBe(33)
+			expect(iterationFunctionCalls[ 13 ].args[ 0 ]).toBe(34)
+			expect(iterationFunctionCalls[ 14 ].args[ 0 ]).toBe(35)
+			expect(iterationFunctionCalls[ 15 ].args[ 0 ]).toBe(36)
+			expect(iterationFunctionCalls[ 16 ].args[ 0 ]).toBe(37)
+			expect(iterationFunctionCalls[ 17 ].args[ 0 ]).toBe(38)
+
+			expect(iterationFunctionCalls[ 18 ].args[ 0 ]).toBe(40)
+			expect(iterationFunctionCalls[ 19 ].args[ 0 ]).toBe(41)
+			expect(iterationFunctionCalls[ 20 ].args[ 0 ]).toBe(42)
+			expect(iterationFunctionCalls[ 21 ].args[ 0 ]).toBe(43)
+			expect(iterationFunctionCalls[ 22 ].args[ 0 ]).toBe(44)
+			expect(iterationFunctionCalls[ 23 ].args[ 0 ]).toBe(45)
+			expect(iterationFunctionCalls[ 24 ].args[ 0 ]).toBe(46)
+			expect(iterationFunctionCalls[ 25 ].args[ 0 ]).toBe(47)
+			expect(iterationFunctionCalls[ 26 ].args[ 0 ]).toBe(48)
+
+			expect(iterationFunctionCalls[ 27 ].args[ 0 ]).toBe(50)
+			expect(iterationFunctionCalls[ 28 ].args[ 0 ]).toBe(51)
+			expect(iterationFunctionCalls[ 29 ].args[ 0 ]).toBe(52)
+			expect(iterationFunctionCalls[ 30 ].args[ 0 ]).toBe(53)
+			expect(iterationFunctionCalls[ 31 ].args[ 0 ]).toBe(54)
+			expect(iterationFunctionCalls[ 32 ].args[ 0 ]).toBe(55)
+			expect(iterationFunctionCalls[ 33 ].args[ 0 ]).toBe(56)
+			expect(iterationFunctionCalls[ 34 ].args[ 0 ]).toBe(57)
+			expect(iterationFunctionCalls[ 35 ].args[ 0 ]).toBe(58)
 		})
 	})
 })

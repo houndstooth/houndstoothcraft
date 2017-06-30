93f88d522f55b909bf7f29231979d30d840a0557
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-30 18:54:17 +0900

    prefer string interpolation over coercing ints into strings with addition

--- src/application/exportFrame.js
+++ src/application/exportFrame.js
@@ -2,6 +2,6 @@ import canvas from '../render/canvas'
 import fileSaver from 'file-saver'
 
 export default () => canvas.toBlob(blob => {
-	fileSaver.saveAs(blob, current.lastSavedFrame + ".png")
+	fileSaver.saveAs(blob, `${current.lastSavedFrame}.png`)
 	current.lastSavedFrame++
 })

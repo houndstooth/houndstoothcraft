c8ba746cc52def06ca6180eefdc8913773f76cc1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-13 08:39:30 +0900

    improvement to hiding canvases when running all tests

--- test/_testSetup.js
+++ test/_testSetup.js
@@ -4,9 +4,9 @@ import clear from '../src/render/clear'
 import testClear from './helpers/testClear'
 
 beforeEach(() => {
-	Object.keys(current.settings.initial).forEach(key => delete current.settings.initial[key])
-	Object.keys(current.settings.iterations).forEach(key => delete current.settings.iterations[key])
-	Object.keys(current.settings.animations).forEach(key => delete current.settings.animations[key])
+	Object.keys(current.settings.initial).forEach(key => delete current.settings.initial[ key ])
+	Object.keys(current.settings.iterations).forEach(key => delete current.settings.iterations[ key ])
+	Object.keys(current.settings.animations).forEach(key => delete current.settings.animations[ key ])
 	current.iterationFrame = 0
 	current.animationFrame = 0
 	current.lastSavedAnimationFrame = 0
@@ -15,14 +15,11 @@ beforeEach(() => {
 	testClear()
 })
 
-describe('clean up (the 2000 may need to be updated as test suite grows)', () => {
-	it('removes canvases when you run the entire test suite', () => {
-		setTimeout(() => {
-			console.log("DIE CANVAS")
-			const place = document.querySelector('.place')
-			if (place) place.parentElement.removeChild(place)
-			const realCanvas = document.querySelector('.realCanvas')
-			if (realCanvas) realCanvas.parentElement.removeChild(realCanvas)
-		}, 2000)
+describe('when you run the entire test suite', () => {
+	it('hides canvases', () => {
+		const place = document.querySelector('.place')
+		if (place) place.style.display = 'none'
+		const realCanvas = document.querySelector('.realCanvas')
+		if (realCanvas) realCanvas.style.display = 'none'
 	})
 })

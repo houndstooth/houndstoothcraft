735a31ed09c270db57734eed1aab575ed28fdf57
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-14 13:55:17 +0900

    brought back canteen for the collapse same colored shapes within tile; ironically a tool for optimizing performance slows tests

--- test/settings/tileSettingsTest.js
+++ test/settings/tileSettingsTest.js
@@ -1,8 +1,10 @@
+import '../../node_modules/canteen/build/canteen.min'
 import execute from '../../src/application/execute'
 import setup from '../../src/application/setup'
 import standardTileIsColors from '../helpers/standardTileIsColors'
 import activateTestMarkerCanvas from '../helpers/activateTestMarkerCanvas'
 import { BLACK, TRANSPARENT } from '../../src/constants'
+import context from '../../src/render/context'
 
 describe('.tileSettings', () => {
 	describe('.tileSize', () => {
@@ -58,20 +60,79 @@ describe('.tileSettings', () => {
 		})
 	})
 
-	xdescribe('.collapseSameColoredShapesWithinTile', () => { //this one will require bringing back that stack looker
+	describe('.collapseSameColoredShapesWithinTile', () => {
+		let overrides
+		beforeEach(() => {
+			context.clear()
+			overrides = {
+				initial: {
+					gridSettings: { gridSize: 1 },
+					colorSettings: { set: [ BLACK, BLACK ] },
+				},
+			}
+		})
 
-	})
+		it('defaults to true, causing tiles whose stripes are the same color to merge into single solid shape', () => {
+			setup({ effects: [], overrides })
+			activateTestMarkerCanvas()
 
-	xdescribe('.isTileUniform', () => {
+			execute()
 
-	})
+			let contextCallStack = context.stack()
+			expect(contextCallStack.length).toBe(8)
+			expect(contextCallStack[ 0 ].attr).toBe('fillStyle')
+			expect(contextCallStack[ 1 ].method).toBe('beginPath')
+			expect(contextCallStack[ 2 ].method).toBe('moveTo')
+			expect(contextCallStack[ 3 ].method).toBe('lineTo')
+			expect(contextCallStack[ 4 ].method).toBe('lineTo')
+			expect(contextCallStack[ 5 ].method).toBe('lineTo')
+			expect(contextCallStack[ 6 ].method).toBe('closePath')
+			expect(contextCallStack[ 7 ].method).toBe('fill')
+		})
 
-	xdescribe('.tileToShapes', () => {
+		it('when set to false, causes the shapes to be rendered separately', () => {
+			overrides.initial.tileSettings = { collapseSameColoredShapesWithinTile: false }
+			setup({ effects: [], overrides })
+			activateTestMarkerCanvas()
 
-	})
+			execute()
 
-	xdescribe('.getCoordinates', () => {
-		// whenTileIsUniform
-		// whenTileIsMultiform
+			let contextCallStack = context.stack()
+			expect(contextCallStack.length).toBe(30)
+
+			expect(contextCallStack[ 0 ].attr).toBe('fillStyle')
+			expect(contextCallStack[ 1 ].method).toBe('beginPath')
+			expect(contextCallStack[ 2 ].method).toBe('moveTo')
+			expect(contextCallStack[ 3 ].method).toBe('lineTo')
+			expect(contextCallStack[ 4 ].method).toBe('lineTo')
+			expect(contextCallStack[ 5 ].method).toBe('closePath')
+			expect(contextCallStack[ 6 ].method).toBe('fill')
+
+			expect(contextCallStack[ 7 ].attr).toBe('fillStyle')
+			expect(contextCallStack[ 8 ].method).toBe('beginPath')
+			expect(contextCallStack[ 9 ].method).toBe('moveTo')
+			expect(contextCallStack[ 10 ].method).toBe('lineTo')
+			expect(contextCallStack[ 11 ].method).toBe('lineTo')
+			expect(contextCallStack[ 12 ].method).toBe('lineTo')
+			expect(contextCallStack[ 13 ].method).toBe('closePath')
+			expect(contextCallStack[ 14 ].method).toBe('fill')
+
+			expect(contextCallStack[ 15 ].attr).toBe('fillStyle')
+			expect(contextCallStack[ 16 ].method).toBe('beginPath')
+			expect(contextCallStack[ 17 ].method).toBe('moveTo')
+			expect(contextCallStack[ 18 ].method).toBe('lineTo')
+			expect(contextCallStack[ 19 ].method).toBe('lineTo')
+			expect(contextCallStack[ 20 ].method).toBe('lineTo')
+			expect(contextCallStack[ 21 ].method).toBe('closePath')
+			expect(contextCallStack[ 22 ].method).toBe('fill')
+
+			expect(contextCallStack[ 23 ].attr).toBe('fillStyle')
+			expect(contextCallStack[ 24 ].method).toBe('beginPath')
+			expect(contextCallStack[ 25 ].method).toBe('moveTo')
+			expect(contextCallStack[ 26 ].method).toBe('lineTo')
+			expect(contextCallStack[ 27 ].method).toBe('lineTo')
+			expect(contextCallStack[ 28 ].method).toBe('closePath')
+			expect(contextCallStack[ 29 ].method).toBe('fill')
+		})
 	})
 })

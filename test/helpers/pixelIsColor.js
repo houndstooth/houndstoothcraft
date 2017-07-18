130f180a201a4f1f3b6274c1822a7503421eec22
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-18 13:01:08 +0900

    lint fix

--- test/helpers/pixelIsColor.js
+++ test/helpers/pixelIsColor.js
@@ -6,9 +6,9 @@ export default (coordinateUnderTest, expectedColor) => {
 	if (actualColor.a === 0 && actualColor.a === expectedColor.a) return true
 
 	for (let i = 0; i < Object.keys(actualColor).length; i++) {
-		const firstColorProperty = Object.entries(actualColor)[i]
-		const firstColorPropertyValue = firstColorProperty[1]
-		const firstColorPropertyKey = firstColorProperty[0]
+		const firstColorProperty = Object.entries(actualColor)[ i ]
+		const firstColorPropertyValue = firstColorProperty[ 1 ]
+		const firstColorPropertyKey = firstColorProperty[ 0 ]
 		const secondColorPropertyValue = expectedColor[ firstColorPropertyKey ]
 		if (!isCloseTo(firstColorPropertyValue, secondColorPropertyValue)) return false
 	}

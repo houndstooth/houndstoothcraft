88c30195b85e2d4acfff9d602a418afed9d6877d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-18 13:50:13 +0900

    skeleton of final settings test

--- test/settings/colorSettingsTest.js
+++ test/settings/colorSettingsTest.js
@@ -1,2 +1,29 @@
-xdescribe('.colorSettings', () => {
+describe('.colorSettings', () => {
+	describe('.set', () => {
+		xit('lets you change the colors of the pattern', () => {
+
+		})
+
+		xit('works for more than two colors', () => {
+
+		})
+	})
+
+	describe('.switcheroo', () => {
+		xit('causes the pattern to turn into strung along teeth flipping directions', () => {
+
+		})
+	})
+
+	describe('.flipGrain', () => {
+		xit('rotates the stripes by 180 degrees, in effect reversing the grain of the pattern', () => {
+
+		})
+	})
+
+	describe('.opacity', () => {
+		xit('affects the alpha of the pixels rendered', () => {
+
+		})
+	})
 })

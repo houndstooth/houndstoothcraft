93a8299d9feb2aa6d38d4d1305bc8bef45047309
Douglas <douglas.blumeyer@gmail.com>
2017-09-26 03:50:56 +0800

    fix some bug with the if blocks, and only check on listeners

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,16 +5,16 @@ webstorm .
 DEV_SERVER_PORT=8080
 KARMA_SERVER_PORT=9876
 
-netstat -an | grep $DEV_SERVER_PORT > /dev/null 2>&1
+netstat -an | grep $DEV_SERVER_PORT | grep LISTEN > /dev/null 2>&1
 if [[ $? -ne 0 ]] ; then
-    npm run integration-test-server > /dev/null 2>&1 &
+    npm start > /dev/null 2>&1 &
 
     sh ./bin/open_tabs.sh $DEV_SERVER_PORT $KARMA_SERVER_PORT
 else
     printf "I think everything has already been started up.\n\n"
 fi
 
-netstat -an | grep $KARMA_SERVER_PORT > /dev/null 2>&1
+netstat -an | grep $KARMA_SERVER_PORT | grep LISTEN > /dev/null 2>&1
 if [[ $? -ne 0 ]] ; then
-    npm start > /dev/null 2>&1 &
+    npm run integration-test-server > /dev/null 2>&1 &
 fi

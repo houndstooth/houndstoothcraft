d8e884167d366b4f28db63ccd473392bb49be6b9
Douglas <douglas.blumeyer@gmail.com>
2017-09-26 04:19:51 +0800

    standardize order

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $OSTYPE == darwin* ]] ; then
-    lsof -ti:8080 -sTCP:LISTEN | xargs kill
     lsof -ti:9876 -sTCP:LISTEN | xargs kill
+    lsof -ti:8080 -sTCP:LISTEN | xargs kill
 else
     PID_TO_KILL=$(netstat -aon | awk '/9879/ {print $5}')
     taskkill //pid $PID_TO_KILL //f

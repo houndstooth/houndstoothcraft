6f2618a21d57f35e08e309177e9dc275cd0d8752
Douglas <douglas.blumeyer@gmail.com>
2017-09-26 03:51:07 +0800

    only kill listeners

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [[ $OSTYPE == darwin* ]] ; then
-    lsof -ti:8080 | xargs kill && lsof -ti:9876 | xargs kill
+    lsof -ti:8080 -sTCP:LISTEN | xargs kill
+    lsof -ti:9876 -sTCP:LISTEN | xargs kill
 else
-    PORT_TO_KILL=$(netstat -aon | awk '/9879/ {print $5}')
-    taskkill //pid $PORT_TO_KILL //f
+    PID_TO_KILL=$(netstat -aon | awk '/9879/ {print $5}')
+    taskkill //pid $PID_TO_KILL //f
 
-    PORT_TO_KILL=$(netstat -aon | awk '/8080/ {print $5}')
-    taskkill //pid $PORT_TO_KILL //f
+    PID_TO_KILL=$(netstat -aon | awk '/8080/ {print $5}')
+    taskkill //pid $PID_TO_KILL //f
 fi

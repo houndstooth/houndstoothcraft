164ef8838c33e66a68d5ab8d7d4f9c8db4615493
Douglas <douglas.blumeyer@gmail.com>
2017-09-28 20:28:35 +0800

    oops actually build to right place

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -3,5 +3,5 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = Object.assign(common, {
 	plugins: [ new UglifyJSPlugin() ],
-	output: { path: __dirname + '/dist', filename: 'bundle.js' },
+	output: { path: __dirname + '/../dist', filename: 'bundle.js' },
 })

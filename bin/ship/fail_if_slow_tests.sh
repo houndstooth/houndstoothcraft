dc63ba7c5e0fcd9f8a27b814e137ed2694d4d67c
Douglas <douglas.blumeyer@gmail.com>
2017-09-30 12:43:33 +0800

    this should have been in with the commit where i muted the slow test reporter for now

--- bin/ship/fail_if_slow_tests.sh
+++ bin/ship/fail_if_slow_tests.sh
@@ -2,6 +2,6 @@
 
 if npm run integration | tee /dev/tty | tee test/unit/slowness-report.txt | grep -q 'SLOW' ; then
    printf "Slow tests detected.\n\n"
-   awk '/SLOW/' test/unit/slowness-report.txt > temp && mv temp test/unit/slowness-report.txt
+#   awk '/SLOW/' test/unit/slowness-report.txt > temp && mv temp test/unit/slowness-report.txt
    exit 1
 fi

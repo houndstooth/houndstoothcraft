7593190ff9ce8f09a1f661f6783a5d566fcacab5
Douglas <douglas.blumeyer@gmail.com>
2017-09-30 19:43:14 +0900

    fix mislabeled variable

--- bin/start/watcher.sh
+++ bin/start/watcher.sh
@@ -2,7 +2,7 @@
 
 WATCHER_PORT=$1
 
-netstat -an | grep $KARMA_SERVER_PORT | grep LISTEN > /dev/null 2>&1
+netstat -an | grep $WATCHER_PORT | grep LISTEN > /dev/null 2>&1
 if [[ $? -ne 0 ]] ; then
     node dev/watcher.js > /dev/null 2>&1 &
 fi

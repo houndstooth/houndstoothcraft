7380020195eafbca4250fcfafb1a1bd7ba7f9e2e
Douglas <douglas.blumeyer@gmail.com>
2017-09-30 19:12:09 +0900

    unnecessary autowatch

--- test/integration/karma.server.js
+++ test/integration/karma.server.js
@@ -3,7 +3,6 @@ const karmaCommonConfig = require('./karma.common')
 module.exports = config => {
 	config.set(Object.assign(karmaCommonConfig, {
 		reporters: [ 'kjhtml' ],
-		autoWatch: true,
 		files: [ 'dist/bundle.js' ],
 	}))
 }

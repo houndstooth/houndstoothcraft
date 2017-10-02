a4aa162b2a18165967463cffd185626e335edcfe
Douglas <douglas.blumeyer@gmail.com>
2017-10-03 08:38:22 +0900

    break up listener to 2 lines lol

--- test/integration/autoRefresh/listener.js
+++ test/integration/autoRefresh/listener.js
@@ -1,1 +1,2 @@
-new EventSource(`http://localhost:${process.env.KARMA_WATCHER_PORT}/codeUpdates`).addEventListener('message', () => window.location.reload())
+const eventSource = new EventSource(`http://localhost:${process.env.KARMA_WATCHER_PORT}/codeUpdates`)
+eventSource.addEventListener('message', () => window.location.reload())

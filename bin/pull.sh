fe0c269f49baf4ae1117f33549bca8c9c2befe85
Douglas <douglas.blumeyer@gmail.com>
2017-10-03 08:40:30 +0900

    scripts for pushing and pulling

---
+++ bin/pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git pull -r
+git submodule foreach git pull -r
--- bin/ship.sh
+++ bin/ship.sh
@@ -5,14 +5,12 @@ set -e
 npm update
 ./node_modules/.bin/eslint . --fix
 
-git pull -r
-git submodule foreach git pull -r
+sh ./bin/pull.sh
 
 sh ./bin/ship/unit_tests_and_cover.sh
 sh ./bin/ship/check_coverage.sh
 sh ./bin/ship/integration_tests_and_fail_if_slow.sh
 
-git submodule foreach git push
-git push
+sh ./bin/push.sh
 
 sh ./bin/ship/deploy.sh

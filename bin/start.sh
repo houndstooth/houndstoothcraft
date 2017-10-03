c8b5cfa89a346106dad64d5fdc888070bf3e1e14
Douglas <douglas.blumeyer@gmail.com>
2017-10-03 09:14:51 +0900

    move config from command line flags into config file

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 sh ./bin/start/karma_watcher.sh
 sh ./bin/start/test_server.sh
 
-CMD="./node_modules/.bin/webpack-dev-server --config build/webpack.dev.js --hot --inline --content-base dist > /dev/null 2>&1 &sh ./bin/start/open_tabs.sh"
+CMD="./node_modules/.bin/webpack-dev-server --config build/webpack.dev.js > /dev/null 2>&1 &sh ./bin/start/open_tabs.sh"
 sh ./bin/start/start_process_if_not_running.sh "$CMD" $DEV_SERVER_PORT
 
 sh ./bin/start/ide.sh
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,6 +1,13 @@
+const webpack = require('webpack')
 const webpackCommonConfig = require('./webpack.common')
 
 module.exports = Object.assign(webpackCommonConfig, {
 	entry: './dev/index.js',
 	devtool: 'eval-source-map',
+	plugins: [ new webpack.HotModuleReplacementPlugin() ],
+	devServer: {
+		contentBase: "dist",
+		hot: true,
+		inline: true,
+	}
 })

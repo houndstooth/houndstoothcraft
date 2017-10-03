2749d7f669e297fff8dd0f4f2d15c1d15a84381e
Douglas <douglas.blumeyer@gmail.com>
2017-10-03 21:30:50 +0900

    ensure if there is more than one listener its ok

--- bin/start/start_process_if_not_running.sh
+++ bin/start/start_process_if_not_running.sh
@@ -3,5 +3,5 @@
 CMD="$1"
 PORT=$2
 
-netstat -an | grep $PORT | grep LISTEN > /dev/null 2>&1
+netstat -an | grep -m1 $PORT | grep LISTEN > /dev/null 2>&1
 if [[ $? -ne 0 ]] ; then $(eval "$CMD") ; fi
--- bin/stop/kill_process_on_port.sh
+++ bin/stop/kill_process_on_port.sh
@@ -7,7 +7,7 @@ if [[ $OSTYPE == darwin* ]] ; then
 else
     netstat -an | grep $PORT | grep LISTEN > /dev/null 2>&1
     if [[ $? -ne 1 ]] ; then
-        PID_TO_KILL=$(netstat -aon | grep $PORT | grep LISTEN | awk '{print $5}')
+        PID_TO_KILL=$(netstat -aon | grep -m1 $PORT | grep LISTEN | awk '{print $5}')
         taskkill //pid $PID_TO_KILL //f
     fi
 fi

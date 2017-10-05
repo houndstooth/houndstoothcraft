dd16dc4be871b06da83194775d9453a11f2c2947
Douglas <douglas.blumeyer@gmail.com>
2017-10-05 15:55:58 +0900

    extract dev server and tabs from start

--- bin/start.sh
+++ bin/start.sh
@@ -6,8 +6,7 @@ sh ./bin/start/watch_unit_tests_and_coverage.sh
 sh ./bin/start/karma_watcher.sh
 sh ./bin/start/test_server.sh
 
-CMD="./node_modules/.bin/webpack-dev-server --config build/webpack.dev.js > /dev/null 2>&1 &sh ./bin/start/open_tabs.sh"
-sh ./bin/start/start_process_if_not_running.sh "$CMD" $DEV_SERVER_PORT
+sh ./bin/start/dev_server_and_tabs.sh
 
 sh ./bin/start/ide.sh
 
---
+++ bin/start/dev_server_and_tabs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+set -e
+
+CMD="./node_modules/.bin/webpack-dev-server --config build/webpack.dev.js > /dev/null 2>&1 &sh ./bin/start/open_tabs.sh"
+
+sh ./bin/start/start_process_if_not_running.sh "$CMD" $DEV_SERVER_PORT

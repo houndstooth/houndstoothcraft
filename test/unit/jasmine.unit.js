2d4787e68353e8c04f0c74533a868dcbbe0b96c5
Douglas <douglas.blumeyer@gmail.com>
2017-10-06 14:15:07 +0900

    jasmine no longer needs to look for js files to run

--- test/unit/jasmine.unit.js
+++ test/unit/jasmine.unit.js
@@ -2,5 +2,5 @@ const jasmineCommonConfig = require('../jasmine.common.js')
 
 module.exports = Object.assign({}, jasmineCommonConfig, {
 	spec_dir: '.',
-	spec_files: [ '**/test/unit/src/**/*.ts', '**/test/unit/src/**/*.js' ],
+	spec_files: [ '**/test/unit/src/**/*.ts' ],
 })

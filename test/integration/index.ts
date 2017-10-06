98e9039d267b6b2790497721a3db604a4ff6f140
Douglas <douglas.blumeyer@gmail.com>
2017-10-06 14:07:54 +0900

    ts for effects

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -3,5 +3,5 @@ declare const require: any
 const testsContext = require.context('./tests', true)
 testsContext.keys().forEach(testsContext)
 
-const effectTestsContext = require.context('../../effects', true, /integration\/.*Test.js$/)
+const effectTestsContext = require.context('../../effects', true, /integration\/.*Test.ts$/)
 effectTestsContext.keys().forEach(effectTestsContext)

5f7363ae32d0e942d9ca51f1189fdf8a6798cc11
Douglas <douglas.blumeyer@gmail.com>
2017-10-06 16:06:46 +0900

    do not load test stuff in dev or prod

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -4,7 +4,6 @@ module.exports = {
 			{
 				test: /\.ts$/,
 				loader: 'awesome-typescript-loader',
-				exclude: /node_modules/,
 			},
 		],
 	},
--- build/webpack.test.js
+++ build/webpack.test.js
@@ -1,5 +1,16 @@
 const webpackCommonConfig = require('./webpack.common')
 
 module.exports = Object.assign({}, webpackCommonConfig, {
+	module: {
+		loaders: [
+			{
+				test: /\.ts$/,
+				loader: 'awesome-typescript-loader',
+				query: {
+					configFileName: './tsconfig.test.json',
+				},
+			},
+		],
+	},
 	output: { path: __dirname + '/../test/integration/dist', filename: 'bundle.js' },
 })

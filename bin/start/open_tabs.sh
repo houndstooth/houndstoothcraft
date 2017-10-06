b318a9b2f5ff05ec2505eee03b5b3203f2061c81
Douglas <douglas.blumeyer@gmail.com>
2017-10-06 20:10:14 +0900

    auto open dev tools for dev tabs

--- bin/start/open_tabs.sh
+++ bin/start/open_tabs.sh
@@ -3,17 +3,37 @@
 set -e
 
 if [[ $OSTYPE == darwin* ]] ; then
-    open -na Google\ Chrome
+    open -na "Google Chrome" --args --auto-open-devtools-for-tabs
 else
-    start chrome
+    start chrome --args --auto-open-devtools-for-tabs
+fi
+sleep 1
+
+declare -a arr=(
+    http://localhost:$DEV_SERVER_PORT
+    http://localhost:$KARMA_SERVER_PORT/debug.html
+)
+
+if [[ $OSTYPE == darwin* ]] ; then
+    for i in ${arr[@]} ; do
+       open $i
+    done
+else
+    for i in ${arr[@]} ; do
+       start chrome $i
+    done
+fi
+
+if [[ $OSTYPE == darwin* ]] ; then
+    open -a "Google Chrome"
+else
+    echo ""
 fi
 sleep 1
 
 declare -a arr=(
     https://app.asana.com/0/358570257763740/list
     https://github.com/houndstooth/web-render
-    http://localhost:$DEV_SERVER_PORT
-    http://localhost:$KARMA_SERVER_PORT/debug.html
     ~/workspace/web-render/test/unit/coverage/lcov-report/index.html
     https://houndstooth.cfapps.io
 )

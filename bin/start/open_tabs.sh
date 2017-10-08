96cd1fd4583cf39e027d5f1763b05afc406044cc
Douglas <douglas.blumeyer@gmail.com>
2017-10-08 20:36:47 +0900

    when home expands with a space, this is necessary

--- bin/start/open_tabs.sh
+++ bin/start/open_tabs.sh
@@ -10,17 +10,17 @@ fi
 sleep 1
 
 declare -a arr=(
-    http://localhost:$DEV_SERVER_PORT
-    http://localhost:$KARMA_SERVER_PORT/debug.html
+    "http://localhost:$DEV_SERVER_PORT"
+    "http://localhost:$KARMA_SERVER_PORT/debug.html"
 )
 
 if [[ $OSTYPE == darwin* ]] ; then
     for i in ${arr[@]} ; do
-       open $i
+       open "$i"
     done
 else
     for i in ${arr[@]} ; do
-       start chrome $i
+       start chrome "$i"
     done
 fi
 
@@ -32,18 +32,18 @@ fi
 sleep 1
 
 declare -a arr=(
-    https://app.asana.com/0/358570257763740/list
-    https://github.com/houndstooth/web-render
-    ~/workspace/web-render/test/unit/coverage/lcov-report/index.html
-    https://houndstooth.cfapps.io
+    "https://app.asana.com/0/358570257763740/list"
+    "https://github.com/houndstooth/web-render"
+    "~/workspace/web-render/test/unit/coverage/lcov-report/index.html"
+    "https://houndstooth.cfapps.io"
 )
 
 if [[ $OSTYPE == darwin* ]] ; then
-    for i in ${arr[@]} ; do
-       open $i
+    for i in "${arr[@]}" ; do
+       open "$i"
     done
 else
-    for i in ${arr[@]} ; do
-       start chrome $i
+    for i in "${arr[@]}" ; do
+       start chrome "$i"
     done
 fi

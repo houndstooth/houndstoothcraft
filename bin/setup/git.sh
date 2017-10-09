bac3949764be3dfab11891bf2050613b62852ba3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-10-09 09:48:15 +0900

    Do not assume me, for git

--- bin/setup/git.sh
+++ bin/setup/git.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-git config user.name \"Douglas Blumeyer\"
-git config user.email douglas.blumeyer@gmail.com
-
 git submodule update --init --recursive
 git submodule foreach git checkout master
-git submodule foreach git config user.name \"Douglas Blumeyer\"
-git submodule foreach git config user.email douglas.blumeyer@gmail.com
 git submodule foreach git pull -r

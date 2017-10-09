0306a34c500ee006581feca75da37cea24122618
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-10-09 18:26:53 +0800

    cool first interactive script

--- bin/setup/git.sh
+++ bin/setup/git.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-git config --get user.name
+git config --get user.name > /dev/null 2>&1
 if [[ $? -ne 0 ]] ; then
     printf "${Yellow}Please set your global git user name: ${NC}"
     read USER_NAME
     git config --global user.name $USER_NAME
 fi
 
-git config --get user.email
+git config --get user.email > /dev/null 2>&1
 if [[ $? -ne 0 ]] ; then
     printf "${Yellow}Please set your global git user email: ${NC}"
     read USER_EMAIL
-    git config --global user.name $USER_EMAIL
+    git config --global user.email $USER_EMAIL
 fi
 
 git submodule update --init --recursive

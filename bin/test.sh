22ff5b2eeabf45726d503738f25fcb58d69a502c
Douglas <douglas.blumeyer@gmail.com>
2017-10-11 19:00:32 +0800

    Silence npm; handle own error messages; share integration webpack output (would be better to only do so if it failed, but thats annoying to do, will do later)

--- bin/test.sh
+++ bin/test.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sh ./bin/test/unit.sh
-sh ./bin/test/integration.sh
+sh ./bin/test/run_command_with_custom_failure_message.sh "./bin/test/unit.sh" "Unit tests failed!"
+sh ./bin/test/run_command_with_custom_failure_message.sh "./bin/test/integration.sh" "Integration tests failed!"
--- bin/test/integration.sh
+++ bin/test/integration.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-webpack --config build/webpack.test.run.js > /dev/null 2>&1
+webpack --config build/webpack.test.run.js
 karma start test/integration/karma.run.js

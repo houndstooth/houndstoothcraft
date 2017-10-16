137bac3fe7a63f684eff274155b2f5587c1f7fff
Douglas <douglas.blumeyer@gmail.com>
2017-10-16 21:40:42 +0800

    you wont necessarily have it in the shell you run it in the first time, but after that, you can use all the bins in your node modules alright

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+sed -i -e "/web-render/d" ~/.bash_profile
+echo 'export PATH=$PATH:~/workspace/web-render/node_modules/.bin' >> ~/.bash_profile
+source ~/.bash_profile
+
 npm i -g npm
 
 sh ./bin/setup/install.sh

59e9b1a844f06aa3ff257e93237235388b79f627
Douglas <douglas.blumeyer@gmail.com>
2017-10-17 20:12:21 +0800

    soon i will create the boundary between units and pixels, but for now, just noticed this little thing

--- src/page/scaleElement.ts
+++ src/page/scaleElement.ts
@@ -1,10 +1,10 @@
 import { Dimension, PageElement } from './types'
 
 const scaleElement: { ({}: { dimensions: Dimension[], element: PageElement }): void } = ({ dimensions, element }) => {
-	element.style.width = inPx(dimensions[ 0 ] as any)
-	element.style.height = inPx(dimensions[ 1 ] as any)
+	element.style.width = inPx(dimensions[ 0 ])
+	element.style.height = inPx(dimensions[ 1 ])
 }
 
-const inPx = (pixelCount: number): string => `${pixelCount}px`
+const inPx = (pixelCount: Dimension): string => `${pixelCount}px`
 
 export default scaleElement

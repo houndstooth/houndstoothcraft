d32ed77d123883fafecdfba5e0bbdd2b6568d195
Douglas <douglas.blumeyer@gmail.com>
2017-10-29 19:56:04 +0800

    put test canvas back into a working state, dont care if types are broken right now

--- test/integration/helpers/activateTestMarkerCanvas.ts
+++ test/integration/helpers/activateTestMarkerCanvas.ts
@@ -1,4 +1,5 @@
-import { Canvas, PageElement, Px } from '../../../src/page'
+// tslint:disable:no-any
+
 import { createCanvasContainer } from '../../../src/page/createCanvasContainer'
 import { scaleElement } from '../../../src/page/scaleElement'
 import { state } from '../../../src/state'
@@ -6,54 +7,46 @@ import { getFromBaseOrDefaultPattern } from '../../../src/store/getFromBaseOrDef
 import { isEmpty } from '../../../src/utilities/codeUtilities'
 import * as to from '../../../src/utilities/to'
 import { NullarySideEffector } from '../../../src/utilities/types'
-import { buildMockElement } from '../../unit/helpers/buildMockElement'
 import { createTestMarkersCanvas } from './createTestMarkersCanvas'
 import { testMarkersClear } from './testMarkersClear'
 
-const prepareCanvasForDisplayInTest: (canvas: Canvas) => void =
-	(canvas: Canvas): void => {
-		if (canvas.style) {
-			canvas.style.display = 'block'
-			canvas.style.position = 'absolute'
-			canvas.style.top = '0'
-			canvas.style.left = '0'
-		}
+const prepareCanvasForDisplayInTest: (canvas: any) => void = canvas => {
+	canvas.style.display = 'block'
+	canvas.style.position = 'absolute'
+	canvas.style.top = '0'
+	canvas.style.left = '0'
+}
+
+const activateTestMarkerCanvas: NullarySideEffector = () => {
+	testMarkersClear()
+
+	let testMarkersCanvas: any = document.querySelector('.test-markers-canvas') || {}
+	if (isEmpty(testMarkersCanvas)) {
+		testMarkersCanvas = createTestMarkersCanvas()
 	}
 
-const activateTestMarkerCanvas: NullarySideEffector =
-	(): void => {
-		testMarkersClear()
+	prepareCanvasForDisplayInTest(testMarkersCanvas)
+	testMarkersCanvas.style.zIndex = '9001'
 
-		let testMarkersCanvas: Canvas = document.querySelector('.test-markers-canvas') || {}
-		if (isEmpty(testMarkersCanvas)) {
-			testMarkersCanvas = createTestMarkersCanvas()
-		}
+	const canvasSize = getFromBaseOrDefaultPattern('canvasSize')
+	testMarkersCanvas.width = canvasSize
+	testMarkersCanvas.height = canvasSize
 
-		prepareCanvasForDisplayInTest(testMarkersCanvas)
-		if (testMarkersCanvas.style) {
-			testMarkersCanvas.style.zIndex = '8001'
-		}
-
-		const canvasSize: Px = getFromBaseOrDefaultPattern('canvasSize')
-		testMarkersCanvas.width = canvasSize
-		testMarkersCanvas.height = canvasSize
-
-		// tslint:disable-next-line:max-line-length
-		const testCanvasDisplayArea: PageElement = document.querySelector('.test-canvas-display-area') || buildMockElement()
-		if (testCanvasDisplayArea.style) {
-			testCanvasDisplayArea.style.display = 'block'
-		}
-
-		scaleElement({ element: testCanvasDisplayArea, dimensions: to.Dimensions([ canvasSize, canvasSize ]) })
-
-		let canvasContainer: PageElement = document.querySelector('.canvas-container') || buildMockElement()
-		if (isEmpty(canvasContainer)) {
-			canvasContainer = createCanvasContainer()
-		}
-
-		prepareCanvasForDisplayInTest(canvasContainer)
-
-		state.mixingDown = true
+	const testCanvasDisplayArea: any = document.querySelector('.test-canvas-display-area') || {}
+	if (testCanvasDisplayArea.style) {
+		testCanvasDisplayArea.style.display = 'block'
 	}
+
+	scaleElement({ element: testCanvasDisplayArea, dimensions: to.Dimensions([ canvasSize, canvasSize ]) })
+
+	let canvasContainer: any = document.querySelector('.canvas-container') || {}
+	if (isEmpty(canvasContainer)) {
+		canvasContainer = createCanvasContainer()
+	}
+
+	prepareCanvasForDisplayInTest(canvasContainer)
+
+	state.mixingDown = true
+}
 
 export { activateTestMarkerCanvas }

1746d34957e9318b4c52e04ba142a28bbb51d2ff
Douglas <douglas.blumeyer@gmail.com>
2017-10-31 19:33:53 +0800

    maybe the changes for windows enslowened this?

--- test/integration/karma.run.js
+++ test/integration/karma.run.js
@@ -3,7 +3,7 @@ const karmaCommonConfig = require('./karma.common')
 module.exports = config => {
 	config.set(Object.assign({}, karmaCommonConfig, {
 		browserNoActivityTimeout: 100000,
-		reportSlowerThan: 250,
+		reportSlowerThan: 300,
 		browsers: [ 'ChromeHeadless' ],
 		singleRun: true,
 	}))

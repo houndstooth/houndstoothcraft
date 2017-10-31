29b71f30736aba82fc039488718e3f03c9a6ab75
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-10-31 23:19:06 +0800

    windows can kill

--- bin/stop.sh
+++ bin/stop.sh
@@ -7,19 +7,23 @@ mkdir -p ./test/integration/dist
 touch ./test/integration/dist/close
 cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > ./test/integration/dist/close
 
-declare -a arr=(
-    $DEV_SERVER_PORT
-    $INTEGRATION_TEST_SERVER_PORT
-    $INTEGRATION_TEST_CODE_UPDATES_WATCHER_PORT
-    $COVERAGE_CODE_UPDATES_WATCHER_PORT
-    $COVERAGE_REPORT_SERVER_PORT
-    $CLOSE_TABS_WATCHER_PORT
-)
+if [[ $OSTYPE == darwin* ]] ; then
+    declare -a arr=(
+        $DEV_SERVER_PORT
+        $INTEGRATION_TEST_SERVER_PORT
+        $INTEGRATION_TEST_CODE_UPDATES_WATCHER_PORT
+        $COVERAGE_CODE_UPDATES_WATCHER_PORT
+        $COVERAGE_REPORT_SERVER_PORT
+        $CLOSE_TABS_WATCHER_PORT
+    )
 
-for i in ${arr[@]} ; do
-    ./bin/stop/kill_process_on_port.sh $i
-done
+    for i in ${arr[@]} ; do
+        ./bin/stop/kill_process_on_port.sh $i
+    done
 
-pkill -f "webpack.test.server.js" || true
+    pkill -f "webpack.test.server.js" || true
+else
+    taskkill //f //im node.exe
+fi
 
 printf "${Purple}Servers down and tabs closed.${NC}\n"

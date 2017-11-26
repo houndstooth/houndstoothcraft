f14c346d31ed7d85cfcc341eec548b809619770c
Douglas <douglas.blumeyer@gmail.com>
2017-11-26 09:06:32 +0900

    thought i did this already, but standardize to webstorm on both mac and d windows

--- bin/setup/install.sh
+++ bin/setup/install.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env sh
 
+set -e
+
 if [[ $OSTYPE == darwin* ]] ; then
     if hash brew 2>/dev/null; then
-        printf "${Blue}Homebrew found.\n${NC}"
+        printf "Homebrew found.\n"
     else
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
 
+    if hash node 2>/dev/null; then
+        printf "Node found.\n"
+    else
+        brew install node
+    fi
+
     if hash webstorm 2>/dev/null; then
-        printf "${Blue}Webstorm found.\n${NC}"
+        printf "Webstorm found.\n"
     else
         brew cask install webstorm
     fi
 
     if hash cf 2>/dev/null; then
-        printf "${Blue}Cloud Foundry CLI found.\n${NC}"
+        printf "Cloud Foundry CLI found.\n"
     else
         brew tap cloudfoundry/tap
         brew install cf-cli
@@ -26,41 +34,20 @@ if [[ $OSTYPE == darwin* ]] ; then
     fi
 
     if [[ -e /Applications/iTerm.app/ ]] ; then
-        printf "${Blue}iTerm2 found.\n${NC}"
+        printf "iTerm2 found.\n"
     else
         brew cask install iterm2
     fi
 
     if [[ -e /Applications/Google\ Chrome.app/ ]] ; then
-        printf "${Blue}Google Chrome found.\n${NC}"
+        printf "Google Chrome found.\n"
     else
         brew cask install google-chrome
     fi
 else
-    type /c/Program\ Files/JetBrains/WebStorm\ 2017.2.4/bin/webstorm > /dev/null 2>&1
-    if [[ $? -ne 0 ]] ; then
-         printf "${Yellow}Missing Webstorm.${NC}\n"
-    else
-         printf "${Blue}Webstorm found.\n${NC}"
-    fi
-
-    if hash cf > /dev/null 2>&1 ; then
-         printf "${Blue}Cloud Foundry CLI and Autopilot plugin found.${NC}\n"
-    else
-         printf "${Yellow}Missing Cloud Foundry CLI and Autopilot plugin.${NC}\n"
-    fi
-
-    type /c/Program\ Files/Git/git-bash.exe > /dev/null 2>&1
-    if [[ $? -ne 0 ]] ; then
-         printf "${Yellow}Missing Git Bash.${NC}\n"
-    else
-         printf "${Blue}Git Bash found.\n${NC}"
-    fi
-
-    type /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe > /dev/null 2>&1
-    if [[ $? -ne 0 ]] ; then
-         printf "${Yellow}Missing Google Chrome.${NC}\n"
-    else
-         printf "${Blue}Google Chrome found.\n${NC}"
-    fi
+    if hash node 2>/dev/null; then printf "Missing Node.\n"; fi
+    if [[ -e /c/Program\ Files/JetBrains/Webstorm\ 2017.2.4/bin/webstorm64.exe ]] ; then printf "Missing Webstorm.\n"; fi
+    if hash cf 2>/dev/null; then printf "Missing Cloud Foundry CLI and Autopilot plugin.\n"; fi
+    if [[ -e /c/Program\ Files/Git/git-bash.exe ]] ; then printf "Missing Git BASH.\n"; fi
+    if [[ -e /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe ]] ; then printf "Missing Google Chrome.\n"; fi
 fi
--- bin/start/ide.sh
+++ bin/start/ide.sh
@@ -5,5 +5,5 @@ set -e
 if hash webstorm 2>/dev/null; then
     webstorm .
 else
-    start "" /c/Program\ Files/JetBrains/WebStorm\ 2017.2.4/bin/webstorm
+    start "" /c/Program\ Files/JetBrains/WebStorm\ 2017.2.4/bin/webstorm64.exe
 fi

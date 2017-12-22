7c51d3f5c94c7604e34de5b52e98a0f250b2e5ce
Douglas <douglas.blumeyer@gmail.com>
2017-12-22 18:10:08 +0900

    yes

--- bin/wip/pull.sh
+++ bin/wip/pull.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-git pull origin wip
+git pull origin wip -r
 git reset HEAD^
 git push origin --delete wip
 
-git submodule foreach git pull origin wip
+git submodule foreach git pull origin wip -r
 git submodule foreach git reset HEAD^
 git submodule foreach git push origin --delete wip

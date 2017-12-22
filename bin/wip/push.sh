6621006b49cf96195f13819fbae1c1fd6508633a
Douglas <douglas.blumeyer@gmail.com>
2017-12-22 18:00:11 +0900

    that should be better

--- bin/wip/push.sh
+++ bin/wip/push.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-git checkout -b wip
-git add .
-git commit -m "wip"
-git push -u origin wip
-git checkout master
-git branch -D wip
-
 git submodule foreach git checkout -b wip
 git submodule foreach git add .
 git submodule foreach git commit -m "wip"
 git submodule foreach git push -u origin wip
 git submodule foreach checkout master
 git submodule foreach branch -D wip
+
+git checkout -b wip
+git add .
+git commit -m "wip"
+git push -u origin wip
+git checkout master
+git branch -D wip

5ad1099c38aca04f3dc862a8f66fea1df71d7af7
Douglas <douglas.blumeyer@gmail.com>
2017-12-22 19:54:57 +0900

    try this

--- bin/wip/pull.sh
+++ bin/wip/pull.sh
@@ -4,8 +4,9 @@ set -e
 
 git pull origin wip -r
 git reset HEAD^
-git push origin --delete wip
 
 git submodule foreach git pull origin wip -r
 git submodule foreach git reset HEAD^
 git submodule foreach git push origin --delete wip
+
+git push origin --delete wip

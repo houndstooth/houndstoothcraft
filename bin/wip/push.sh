1f8a75e450ea56ea21181c5a58f651399300680c
Douglas <douglas.blumeyer@gmail.com>
2018-01-05 20:47:38 +0900

    maybe this is more efficient, and less risky that one submodule will mess with another

--- bin/wip/push.sh
+++ bin/wip/push.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
-git submodule foreach git checkout -b wip
-git submodule foreach git add .
-git submodule foreach git commit -m "wip"
-git submodule foreach git push origin wip
-git submodule foreach git checkout master
-git submodule foreach git branch -D wip
+git submodule foreach "
+    git checkout -b wip
+    git add .
+    git commit -m 'wip'
+    git push origin wip
+    git checkout master
+    git branch -D wip
+"
 
 git checkout -b wip
 git add .
-git commit -m "wip"
+git commit -m 'wip'
 git push origin wip
 git checkout master
 git branch -D wip

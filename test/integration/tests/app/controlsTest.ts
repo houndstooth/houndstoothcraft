ad7c2ac8c77b34832dc8412e9cd94601d82c95c3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-01-06 18:15:00 +0900

    effect toggles test doesnt have to do so much setup now that we're using very similar html to actual app

--- test/integration/tests/app/controlsTest.ts
+++ test/integration/tests/app/controlsTest.ts
@@ -5,34 +5,15 @@ import {
 	appState,
 	BLACK,
 	createEffectToggles,
-	enableOrDisableOtherEffectToggles,
 	patternState,
 	to,
 	TRANSPARENT,
-	updateCurrentFrame,
 } from '../../../../src/indexForTest'
 import { createMockElement } from '../../../unit'
 
 describe('controls', () => {
 	describe('effect toggles', () => {
-		beforeEach(() => {
-			spyOn(updateCurrentFrame, 'default')
-			spyOn(enableOrDisableOtherEffectToggles, 'default')
-
-			const descriptionsContainer: HTMLElement = document.createElement('div')
-			descriptionsContainer.setAttribute('id', 'descriptions-container')
-			descriptionsContainer.style.display = 'none'
-			document.body.appendChild(descriptionsContainer)
-			appState.dom.descriptionsContainer = descriptionsContainer
-		})
-
 		it('attaches handlers which cause the setting of the main houndstooth to change based on the effect', () => {
-			const effectTogglesContainer: HTMLElement = document.createElement('div')
-			effectTogglesContainer.setAttribute('id', 'effect-toggles-container')
-			effectTogglesContainer.style.display = 'none'
-			document.body.appendChild(effectTogglesContainer)
-			appState.dom.effectTogglesContainer = effectTogglesContainer
-
 			appState.settings.availableEffects = { gongram: gongramEffect }
 
 			createEffectToggles.default(Object.values(effects))

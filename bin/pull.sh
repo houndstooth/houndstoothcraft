23d807d78b67ac3ace8ee5c4390a33a0a4274fdb
Douglas <douglas.blumeyer@gmail.com>
2018-01-06 13:45:36 +0900

    okay it looks like the real problem was that set -e fails on any non-zero exit code, and since git pull exits 127 if it already up-to-date, we have to not pull unless there is something to pull

--- bin/pull.sh
+++ bin/pull.sh
@@ -2,6 +2,14 @@
 
 set -e
 
-git pull -r
+function pull_if_needed {
+    if [[ $(git rev-list --left-right --count origin/master...@ | cut -f1) != 0 ]] ; then
+        git pull -r
+    fi
+}
+
+export -f pull_if_needed
+
+pull_if_needed
 npm i
-git submodule foreach git pull -r
+git submodule foreach pull_if_needed

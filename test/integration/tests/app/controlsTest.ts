020b897ede8fbb0d2374d81973f56d250a5d54ea
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-02-08 08:41:01 +0900

    test conflicting effect toggles
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- test/integration/tests/app/controlsTest.ts
+++ test/integration/tests/app/controlsTest.ts
@@ -3,17 +3,11 @@
 import * as effects from '../../../../effects'
 import { GONGRAM_COLOR_SET } from '../../../../effects/gongram/constants'
 import {
-	appState,
-	BLACK,
-	createEffectToggles,
-	Frame,
-	NamedEffect,
-	patternState,
-	setupAvailableEffects,
-	to,
+	appState, BLACK, createEffectToggles, Frame, NamedEffect, patternState, setupAvailableEffects, to,
 	TRANSPARENT,
 } from '../../../../src/indexForTest'
 import { expectStandardPattern } from '../../helpers'
+import { getGinghamChevronContinuumStripePositions } from '../../../../effects/gingham-chevron-continuum/pattern'
 
 describe('controls', () => {
 	const availableEffects: NamedEffect[] = Object.values(effects)
@@ -34,7 +28,7 @@ describe('controls', () => {
 		})
 
 		it('keeps animating if the selection of effects changes while animation is playing, as long as the new effect selection is still animatable', () => {
-			appState.dom.effectToggles['gingham-chevron-continuum'].click()
+			appState.dom.effectToggles[ 'gingham-chevron-continuum' ].click()
 
 			appState.dom.playButton.click()
 			expect(appState.controls.animating).toBe(true)
@@ -49,12 +43,12 @@ describe('controls', () => {
 		})
 
 		it('stops animating if the selection of effects changes while animation is playing to a selection which is not animatable', () => {
-			appState.dom.effectToggles['gingham-chevron-continuum'].click()
+			appState.dom.effectToggles[ 'gingham-chevron-continuum' ].click()
 			appState.dom.playButton.click()
 			expect(appState.controls.animating).toBe(true)
 
 			const currentFrame: Frame = appState.controls.currentFrame
-			appState.dom.effectToggles['gingham-chevron-continuum'].click()
+			appState.dom.effectToggles[ 'gingham-chevron-continuum' ].click()
 			expect(appState.controls.animating).toBe(false)
 			expect(appState.controls.currentFrame).toBe(currentFrame)
 
@@ -64,6 +58,14 @@ describe('controls', () => {
 			expect(appState.dom.rewindButton.disabled).toBe(true)
 
 			expectStandardPattern()
+		})
+
+		it('should not select the effect if it conflicts with another already selected effect', () => {
+			appState.dom.effectToggles[ 'gingham-chevron-continuum' ].click()
+			expect(patternState.stripeSettings.stripePositionSettings.getStripePositions).toBe(getGinghamChevronContinuumStripePositions.default)
+
+			appState.dom.effectToggles[ 'harmonitooth' ].click()
+			expect(patternState.stripeSettings.stripePositionSettings.getStripePositions).toBe(getGinghamChevronContinuumStripePositions.default)
 		})
 	})
 })
